0a65dbe32cc15bcb5711f09b7aae5eff9057d6c5
Robert Hudspeth <hudspero@onid.orst.edu>
2015-09-11 13:58:36 -0700

    Added User Validation Function Header

--- BigML.hpp
+++ BigML.hpp
@@ -2,7 +2,7 @@
 //  BigML.hpp
 //
 //  Created by Robert Lee Hudspeth
-//  Last Modified on 9/4/15
+//  Last Modified on 9/11/15
 //
 
 #ifndef BIGML_HPP
@@ -22,7 +22,6 @@
 using namespace std::this_thread;
 using namespace std::chrono;
 using std::cout;
-using std::cin;
 using std::endl;
 using std::string;
 using std::getenv;
@@ -33,30 +32,32 @@ class BigML {
 
     private:
 	
-	string user_name, user_key, BIGML_AUTH; //For user authentication
-    	string request, resource, iden, response; //For building request URLs
-	int wait; //For delaying fetch requests
-	int code; //For storing status code
-	int i, j; //For index variables
+		string user_name, user_key, BIGML_AUTH; //For user authentication
+        string request, resource, iden, response; //For building request URLs
+		int wait; //For delaying fetch requests
+		int code; //For storing status code
+		int i, j; //For index variables
 	
-	CURL *curl; //libcURL easy_handle for easy operations
+		CURL *curl; //libcURL easy_handle for easy operations
 
-	cJSON *root, *data, *item; //For submitting JSON body
-	char *json;		   //For submitting JSON body
+		cJSON *root, *data, *item; //For submitting JSON body
+		char *json;				   //For submitting JSON body
 	
-	struct str s;			     //For storing JSON response body
-	string src, dts, ano, cls, ens, mod; //For storing/fetching resource identifiers
+		struct str s;						 //For storing JSON response body
+		string src, dts, ano, cls, ens, mod; //For storing/fetching resource identifiers
 	
-	void get_source_identifier(str *s);
-	void get_dataset_identifier(str *s);
-	void get_anomaly_identifier(str *s);
-	void get_cluster_identifier(str *s);
-	void get_ensemble_identifier(str *s);
-	void get_model_identifier(str *s);
+		void validate(string &BIGML_AUTH);
 	
-	void get_status(str *s);
-	void get_dataset_status(str *s);
-	void get_prediction_info(str *s);
+		void get_source_identifier(str *s);
+		void get_dataset_identifier(str *s);
+		void get_anomaly_identifier(str *s);
+		void get_cluster_identifier(str *s);
+		void get_ensemble_identifier(str *s);
+		void get_model_identifier(str *s);
+	
+		void get_status(str *s);
+		void get_dataset_status(str *s);
+		void get_prediction_info(str *s);
 	
 /**********************************************************************************************/
 	
@@ -76,18 +77,18 @@ class BigML {
 	string get_ens();
 	string get_mod();
 	
-    	void create_remote_source(const char* url, const char* title);
+        void create_remote_source(const char* url, const char* title);
 	
 		void get_source(const string source_iden);
-	
+
 	void create_dataset(const string source_iden);
-	
+
 		void get_dataset(const string dataset_iden);
-	
+
 	void create_anomalies(const string dataset_iden);
 	
 		void get_anomalies(const string anomaly_iden);
-	
+
 	void create_cluster(const string dataset_iden);
 			
 		void get_cluster(const string cluster_iden);
@@ -99,8 +100,8 @@ class BigML {
 	void create_model(const string dataset_iden);
 	
 		void get_model(const string model_iden);
-	
-	void create_prediction(const string model_iden, const char *col, const float num);
+
+        void create_prediction(const string model_iden, const char *col, const float num);
 	
 /**********************************************************************************************/
 
